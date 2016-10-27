45054d8fe6b7640f52a4d4b1799910045eb60fd7
kevinthon <kevin.thon@gmail.com>
2016-10-27 09:32:12 +0200

    Påbegynt dokumentasjon av datasett

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Antallet med brystbevarende kirurgi og antallet potensielle med tumorstørrelse
+#' 0-30 mm pr. sykehus 2013-2015
+#'
+#' En list som består av to datarammer, AntTilfeller og AntTotalt. Den første angir
+#' antallet som har fått brystbevarende kirugi for alle aktuelle sykehus i årene
+#' 2013-2015, den andre angir antallet som var aktuelle for det.
+#'
+#' @format To datarammer med 21 rader og 3 kolonner
+#' \itemize{
+#'   \item AntTilfeller: Radnavn er sykehusnavn, kolonnene er
+#'    \itemize{
+#'    \item 2013: Antall tilfeller i 2013
+#'    \item 2014: Antall tilfeller i 2014
+#'    \item 2015: Antall tilfeller i 2015
+#'   }
+#'   \item AntTotalt: Radnavn er sykehusnavn, kolonnene er
+#'    \itemize{
+#'    \item 2013: Antall tilfeller i 2013
+#'    \item 2014: Antall tilfeller i 2014
+#'    \item 2015: Antall tilfeller i 2015
+#'   }
+#' }
+"Brystbevarende_sh"
